1a030ccf99e0359f0f32483dd60bf3a446849837
Mathews Motta <mathewscsmotta@gmail.com>
2020-04-12 03:05:13 +0100

    First draw with a static image

--- README.md
+++ README.md
@@ -1,5 +1,13 @@
 # Moon Pong
-A Pong experiment made in Lua
+
+The goal of this project is to experiment and learn about [Love 2D](https://love2d.org/), a simplified game engine made in [Lua](https://www.lua.org/), a programming language created in [PUC-Rio](https://www.puc-rio.br/index.html), one of the most famous universities in Rio de Janeiro, Brazil.
+
+"Lua" means "moon" in portuguese, which is where the project name is inspired uppon. It is a data-drive language and was originally intended for embedded use in larger application and is popular in the video game industry.
+
+# The Game
+
+# Love2D Concepts
+
+## Hitboxes
 
 
-(Projeto em Pausa)
---
+++ src/main.lua
@@ -0,0 +1,47 @@
+push = require 'push'
+
+WINDOW_WIDTH = 1280
+WINDOW_HEIGHT = 720
+
+VIRTUAL_WIDTH = 432
+VIRTUAL_HEIGHT = 243
+
+PADDLE_SPEED = 200
+
+function love.load()
+    love.graphics.setDefaultFilter('nearest', 'nearest')
+
+    smallFont = love.graphics.newFont('font.ttf', 8)
+    scoreFont = love.graphics.newFont('font.ttf', 32)
+    love.graphics.setFont(smallFont)
+
+    push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
+        fullscreen = false,
+        resizable = false,
+        vsync = true
+    })
+
+    player1Score = 0
+    player2Score = 0
+
+    player1Y = 30
+    player2Y = VIRTUAL_HEIGHT - 50
+end
+
+function love.draw()
+    push:apply('start')
+
+    love.graphics.clear(40, 45, 52, 0, 0)
+
+    love.graphics.rectangle('fill', 10, 30, 5, 20)
+    love.graphics.rectangle('fill', VIRTUAL_WIDTH - 10, VIRTUAL_HEIGHT - 50, 5, 20)
+    love.graphics.rectangle('fill', VIRTUAL_WIDTH / 2 - 2, VIRTUAL_HEIGHT / 2 - 2, 4, 4)
+
+    push:apply('end')
+end
+
+function love.keypressed(key)
+    if key == 'escape' then
+        love.event.quit()
+    end
+end
